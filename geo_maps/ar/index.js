0b7b0ec8f41b273554eae32345dba3782efa70ce
geoeguren <geoeguren@gmail.com>
2026-05-22 14:30:29 -0300

    Update index.js

--- geo_maps/ar/index.js
+++ geo_maps/ar/index.js
@@ -6,12 +6,14 @@ import { PROVINCIAS_MAP_AR }    from './provincias.js';
 import { DEPARTAMENTOS_MAP_AR } from './departamentos.js';
 import { LOCALIDADES_MAP_AR }   from './localidades.js';
 import { MUNICIPIOS_MAP_AR }    from './municipios.js';
+import { REGIONES_MAP_AR }      from './regiones.js';
 
-export { PROVINCIAS_MAP_AR, DEPARTAMENTOS_MAP_AR, LOCALIDADES_MAP_AR, MUNICIPIOS_MAP_AR };
+export { PROVINCIAS_MAP_AR, DEPARTAMENTOS_MAP_AR, LOCALIDADES_MAP_AR, MUNICIPIOS_MAP_AR, REGIONES_MAP_AR };
 
 export const AR_GEO_MAPS = {
   provincias:    PROVINCIAS_MAP_AR,
   departamentos: DEPARTAMENTOS_MAP_AR,
   localidades:   LOCALIDADES_MAP_AR,
   municipios:    MUNICIPIOS_MAP_AR,
+  regiones:      REGIONES_MAP_AR,
 };
