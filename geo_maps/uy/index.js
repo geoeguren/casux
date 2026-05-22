84859e70b739d0d458a536a3930fb985fbf735f1
geoeguren <geoeguren@gmail.com>
2026-05-22 14:30:43 -0300

    Update index.js

--- geo_maps/uy/index.js
+++ geo_maps/uy/index.js
@@ -4,10 +4,12 @@
 
 import { DEPARTAMENTOS_MAP_UY } from './departamentos.js';
 import { MUNICIPIOS_MAP_UY }    from './municipios.js';
+import { REGIONES_MAP_UY }      from './regiones.js';
 
-export { DEPARTAMENTOS_MAP_UY, MUNICIPIOS_MAP_UY };
+export { DEPARTAMENTOS_MAP_UY, MUNICIPIOS_MAP_UY, REGIONES_MAP_UY };
 
 export const UY_GEO_MAPS = {
   departamentos: DEPARTAMENTOS_MAP_UY,
   municipios:    MUNICIPIOS_MAP_UY,
+  regiones:      REGIONES_MAP_UY,
 };
