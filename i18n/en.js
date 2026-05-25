6ddc1b8e28101b66a984367fa0bec3e0f3653454
geoeguren <geoeguren@gmail.com>
2026-05-25 17:04:25 -0300

    Update en.js

--- i18n/en.js
+++ i18n/en.js
@@ -347,6 +347,7 @@ window.I18N_EN = {
   validate_export_no_layers:            'No layers loaded to export.',
   validate_basemap_invalid:             'Unknown basemap.',
   validate_layer_too_many_features:     '{titulo} is too large ({n}) to display in full. Ask for a specific region or province.',
+  validate_layer_may_be_slow:           '{titulo} ({n} features) may take a few seconds to load. Continuing anyway.',
   validate_no_active_layers_to_add:     'No layers loaded on the map to add to.',
   validate_layer_already_on_map:        'That layer is already on the map.',
   validate_filter_no_filterable_fields: 'This layer has no filterable fields defined.',
